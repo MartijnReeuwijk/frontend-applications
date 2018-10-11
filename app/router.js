ec4f9acddb6ec29eb57d72dfb3f26ea4552f7fba
Martijn <m.reeuwijk@Live.nl>
2018-10-11 11:06:18 +0200

    First commit basic set up.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('risicoanalyse');
+  this.route('mainnav');
 });
 
 export default Router;
---
+++ app/routes/risicoanalyse.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model(){
+    return ["Item1","Item2","Item3","Item4"];
+  }
+});
